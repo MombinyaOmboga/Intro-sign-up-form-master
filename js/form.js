2d2754781b770c2772ce48dd665e3f7d961cdc27
Mombinya <ombogas.developer@gmail.com>
2024-03-22 23:42:34 +0300

    Added form validation and error handling for signup form

--- js/form.js
+++ js/form.js
@@ -5,9 +5,13 @@ document
 
           //reset error messages
           document.getElementById('fname-error').textContent = ''
+          document.getElementById('firstname').classList.remove('invalid')
           document.getElementById('lname-error').textContent = ''
+          document.getElementById('lastname').classList.remove('invalid')
           document.getElementById('email-error').textContent = ''
+          document.getElementById('email').classList.remove('invalid')
           document.getElementById('password-error').textContent = ''
+          document.getElementById('password').classList.remove('invalid')
 
           //fetch form inputs
           var firstName = document.getElementById('firstname').value
@@ -18,41 +22,53 @@ document
           //Form Validation
           if (firstName.trim() === '') {
             document.getElementById('fname-error').textContent =
-              'First Name cannot be empty'
-            console.log(document.getElementById('fname-error'))
+              'Provide first name!'
+            document.getElementById('firstname').classList.add('invalid')
             return
           }
           if (lastName.trim() === '') {
             document.getElementById('lname-error').textContent =
-              'Last name cannot be empty'
+              'Provide second name!'
+
+            document.getElementById('lastname').classList.add('invalid')
             return
           }
 
           if (email.trim() === '') {
             document.getElementById('email-error').textContent =
               'Enter your email'
+            document.getElementById('email').classList.add('invalid')
             return
           } else if (!isValidEmail(email)) {
             document.getElementById('email-error').textContent =
               'Enter a valid email!'
+
             return
           }
 
           if (password.trim() === '') {
             document.getElementById('password-error').textContent =
-              'Enter your password'
+              'Enter password!'
+            document.getElementById('password').classList.add('invalid')
             return
           } else if (password.length < 8) {
             document.getElementById('password-error').textContent =
               'Password too short!'
+            document.getElementById('password').classList.add('invalid')
+            return
+          }else if(!isValidPassword(password)){
+            document.getElementById('password-error').textContent = 'Password weak!'
             return
           }
-
-          // this.onsubmit(); // No need for this line, as it's redundant
         })
 
-      //Check email validity
+      //Check email nad password validity
       function isValidEmail(email) {
         var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
         return emailRegex.test(email)
+      }
+
+      function isValidPassword(password){
+        var passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
+        return passwordRegex.test(password)
       }
